9bf07edc01f165d52ff353c4cc49c6451a54539c
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2024-04-03 19:23:57 -0300

    completing bottom bar component

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,9 +1,60 @@
+"use client"
+
+import { SignOutButton, SignedIn } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import { sidebarLinks } from "@/constants";
 
 function Bottombar(){
+  const router = useRouter();
+  const pathname = usePathname();
+
   return (
-    <h1>
-      Bottombar
-    </h1>
+    <section className="bottombar">
+      <div className="bottombar_container">
+      {sidebarLinks.map((link) => {
+
+const isActive = (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route
+return (
+  <Link 
+    href = {link.route}
+    key= {link.label}
+    className = {`bottombar_link ${isActive && 'bg-primary-500'}`}
+  >
+    <Image 
+      src={link.imgURL}
+      alt = {link.label}
+      width={24}
+      height={24}
+    />
+
+    <p className="text-light-1 max-lg:hidden">{link.label.split(/\s+/)[0] }</p>
+  </Link>
+)}
+)}
+</div>
+
+<div className="mt-10 px-6">
+<SignedIn>
+  <SignOutButton signOutCallback={() =>router.push('/sign-in')}>
+      <div className="flex cursor-pointer gap-4 p-4">
+          <Image 
+          src="/assets/logout.svg"
+          alt ="logout"
+          width={24}
+          height={24}
+          />
+        
+        <p className="text-light-2 max-sm:hidden">Logout</p>
+
+      </div>
+      </SignOutButton>
+       </SignedIn>
+
+      </div>
+
+    </section>
   )
 };
 
