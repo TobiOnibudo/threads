82221e1e979e3aa309ece65f734086afe7aa9bab
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2024-04-14 23:54:22 -0300

    Setting up files for database connection

---
+++ lib/mongoose.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose'
+
+//mongoose connection status
+let isConnected = false; 
+
+export const connectToDB = async() => {
+    mongoose.set('strictQuery',true);
+    if (!process.env.MONGODB_URL) return console.log('MONGODB_URL not found');
+    if(isConnected) return console.log('Already connected to MonogoDB')
+
+    try{
+
+    }
+    catch(error)
+    {
+        
+    }
+}
