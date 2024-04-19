4dda9697188b63e9cb102edf86bdefa9e1dde1d5
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2024-04-18 22:38:50 -0300

    Fetching threads functionality completed

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -32,3 +32,35 @@ interface Params {
     throw new Error(`Error creating thread: ${error.message}`)
 }
 }
+
+export async function fetchThreads(pageNumber= 1, pageSize = 20){
+    connectToDB();
+
+    //calculate the number of posts to skip
+    const skipAmount = (pageNumber - 1)* pageSize
+
+    // fetch Threads with no comments
+    const postsQuery = Thread.find({parentId: {$in: [null,undefined]}})
+    .sort({createdAt: 'desc'})
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({path: 'author',model: User})
+    .populate({path:'children', 
+    populate:{
+        path: 'author',
+        model: User,
+        select: '_id name parentId image'
+    } })
+
+
+    const totalPostsCount = await Thread.countDocuments({parentId: {$in: [null,undefined]}})
+
+    const posts = await postsQuery.exec()
+
+    const isNext = totalPostsCount > skipAmount + posts.length ;
+
+    return {
+        posts,
+        isNext 
+    }
+}
