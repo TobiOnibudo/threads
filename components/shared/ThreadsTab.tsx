475a0cfa134305e01c3e3f1dd334aee78b43f3a6
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2024-04-22 22:48:57 -0300

    Adding Threads tabs

---
+++ components/shared/ThreadsTab.tsx
@@ -0,0 +1,19 @@
+interface Props{
+    currentUserId: string;
+    accountId : string;
+    accountType : string;
+}
+
+async function ThreadsTab({
+    currentUserId,
+    accountId, 
+    accountType
+} : Props){
+  return (
+    <section>
+      ThreadsTab
+    </section>
+  )
+};
+
+export default ThreadsTab; 
