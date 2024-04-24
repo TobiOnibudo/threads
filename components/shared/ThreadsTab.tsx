01f057f67ff7366aa73c48c0f9d00c2cb14b8f7c
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2024-04-23 21:36:35 -0300

    populating  data on the frontend

--- components/shared/ThreadsTab.tsx
+++ components/shared/ThreadsTab.tsx
@@ -1,3 +1,7 @@
+import { fetchUserThreads } from "@/lib/actions/user.actions";
+import { redirect } from "next/navigation";
+import ThreadCard from "../cards/ThreadCard";
+
 interface Props{
     currentUserId: string;
     accountId : string;
@@ -9,9 +13,27 @@ async function ThreadsTab({
     accountId, 
     accountType
 } : Props){
+let result = await fetchUserThreads(accountId)
+
+if(!result) redirect('/')
+
   return (
-    <section>
-      ThreadsTab
+    <section className="mt-9 flex flex-col gap-10">
+      {result.threads.map((thread : any) =>{
+       return ( <ThreadCard 
+        key ={thread._id}
+        id = {thread._id}
+        currentUserId = {currentUserId}
+        parentId = {thread.parentId}
+        content ={thread.text}
+        author = {accountType === 'User' ? 
+        {name : result.name, image : result.image, id: result.id} 
+        : {name: thread.author.name, image : thread.author.image, id : thread.author.id}}
+        community = {thread.community}
+        createdAt = {thread.createdAt}
+        comments  = {thread.children}
+        />)
+      })}
     </section>
   )
 };
