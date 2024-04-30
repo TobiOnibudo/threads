48c3376faaf3e67c421ce111ec5a2be388bc0201
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2024-04-29 23:54:24 -0300

    adding community into the UI

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -15,13 +15,14 @@ import { z } from "zod"
 import { ThreadValidation } from '@/lib/validations/thread';
 import  {createThread}  from '@/lib/actions/thread.actions'
 import { usePathname, useRouter} from 'next/navigation'
+import { useOrganization } from '@clerk/nextjs'
 
 
 
 function PostThread({userId} : {userId : string}){ 
   const router = useRouter();
   const pathname = usePathname();
-
+  const {organization}  = useOrganization();
 
   const form = useForm({
         resolver : zodResolver(ThreadValidation),
@@ -35,7 +36,7 @@ function PostThread({userId} : {userId : string}){
         await createThread({
           text: values.thread,
           author: userId,
-          communityId: null,
+          communityId: organization ? organization.id : null,
           path: pathname,
         })
 
