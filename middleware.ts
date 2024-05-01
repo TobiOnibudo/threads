739c962597dd7b9026c2648d90cce0a28772991b
Tobi Onibudo <Tobi.D.Onibudo@gmail.com>
2024-05-01 10:53:29 -0300

    Switching back to requiring authentication on landing page

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // for more information about configuring your Middleware
 export default authMiddleware({
   // Allow signed out users to access the specified routes:
-  publicRoutes: ["/","/api/webhook/clerk","/api/uploadthing","/"],
+  publicRoutes: ["/api/webhook/clerk","/api/uploadthing",],
 
   // An array of routes to be ignored by the authentication middleware.
   ignoredRoutes: ["/api/webhook/clerk", "/api/uploadthing"],
